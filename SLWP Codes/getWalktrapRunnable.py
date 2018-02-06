f70128f67383b51bf22810e7c93e483c1f4213f5
stilllearningsoumya <sjituit@gmail.com>
2018-02-06 14:37:38 +0530

    run all script for slwp

---
+++ SLWP Codes/getWalktrapRunnable.py
@@ -0,0 +1,19 @@
+from createWeightsForSLWP import createWeights
+from validateCluster import runToValidate
+from formNetwork import runToCreateNetwork
+from createWalktrapInput import formWalktrapInput
+
+#######################Running SLWP###############################
+createWeights()
+runToValidate()
+runToCreateNetwork()
+formWalktrapInput()
+##################################################################
+######################Run Map Program#############################
+##################################################################
+######################Run Walktrap################################
+##################################################################
+######################Run Reverse Map#############################
+##################################################################
+######################Evaluate####################################
+##################################################################
